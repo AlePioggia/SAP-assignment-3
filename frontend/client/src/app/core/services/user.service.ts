4a8947303da6d5e7612105fc0210baf1b0c68102
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-01-20 16:55:01 +0100

    feat: adapt front-end

--- frontend/client/src/app/core/services/user.service.ts
+++ frontend/client/src/app/core/services/user.service.ts
@@ -8,15 +8,23 @@ import { User, UserRegistrationRequest, UserAuthenticationRequest, UserResponse 
 })
 export class UserService {
   private apiUrl = 'http://localhost:9000/api/user';
+  cookieName: string = 'jwt_token';
 
   constructor(private http: HttpClient) {}
+
+  setToken(token: string): void {
+    localStorage.setItem(this.cookieName, token);
+  }
 
   registerUser(request: UserRegistrationRequest): Observable<string> {
     return this.http.post<string>(`${this.apiUrl}/register`, request);
   }
 
-  authenticateUser(request: UserAuthenticationRequest): Observable<{ userId: string }> {
-    return this.http.post<{ userId: string }>(`${this.apiUrl}/authenticate`, request);
+  authenticateUser(request: UserAuthenticationRequest) {
+    const response = this.http.post<{ userId: string }>(`${this.apiUrl}/authenticate`, request);
+    response.subscribe(data => {
+      this.setToken(data.userId);
+    });
   }
 
   getUserById(id: string): Observable<UserResponse> {
